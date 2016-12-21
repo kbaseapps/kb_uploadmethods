e62bcf7c72b0b4fa24d708a9d947d38a98a570a9
Tianhao Gu <Tian@mcswl086.mcs.anl.gov>
2016-12-21 13:09:08 -0600

    update

---
+++ lib/src/us/kbase/kbuploadmethods/UploadMethodResult.java
@@ -0,0 +1,60 @@
+
+package us.kbase.kbuploadmethods;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * <p>Original spec-file type: UploadMethodResult</p>
+ * 
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("com.googlecode.jsonschema2pojo")
+@JsonPropertyOrder({
+    "obj_ref"
+})
+public class UploadMethodResult {
+
+    @JsonProperty("obj_ref")
+    private String objRef;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("obj_ref")
+    public String getObjRef() {
+        return objRef;
+    }
+
+    @JsonProperty("obj_ref")
+    public void setObjRef(String objRef) {
+        this.objRef = objRef;
+    }
+
+    public UploadMethodResult withObjRef(String objRef) {
+        this.objRef = objRef;
+        return this;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperties(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        return ((((("UploadMethodResult"+" [objRef=")+ objRef)+", additionalProperties=")+ additionalProperties)+"]");
+    }
+
+}
