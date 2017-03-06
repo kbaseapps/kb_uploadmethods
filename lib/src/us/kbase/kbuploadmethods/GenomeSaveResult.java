8c9e0313770cb4fae1fba7d88ead8de06bdefc17
Tianhao-Gu <tgu@anl.gov>
2017-03-06 16:37:59 -0600

    add genbank importer

---
+++ lib/src/us/kbase/kbuploadmethods/GenomeSaveResult.java
@@ -0,0 +1,60 @@
+
+package us.kbase.kbuploadmethods;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * <p>Original spec-file type: GenomeSaveResult</p>
+ * 
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("com.googlecode.jsonschema2pojo")
+@JsonPropertyOrder({
+    "genome_ref"
+})
+public class GenomeSaveResult {
+
+    @JsonProperty("genome_ref")
+    private String genomeRef;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("genome_ref")
+    public String getGenomeRef() {
+        return genomeRef;
+    }
+
+    @JsonProperty("genome_ref")
+    public void setGenomeRef(String genomeRef) {
+        this.genomeRef = genomeRef;
+    }
+
+    public GenomeSaveResult withGenomeRef(String genomeRef) {
+        this.genomeRef = genomeRef;
+        return this;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperties(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        return ((((("GenomeSaveResult"+" [genomeRef=")+ genomeRef)+", additionalProperties=")+ additionalProperties)+"]");
+    }
+
+}
