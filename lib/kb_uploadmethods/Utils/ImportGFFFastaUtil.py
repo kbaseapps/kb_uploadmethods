21fa96f5a45b19eddd3f1334d464454c9c651a46
JamesJeffryes <jamesgjeffryes@gmail.com>
2018-07-11 16:39:49 -0500

    use beta GFU for both genome uploaders

--- lib/kb_uploadmethods/Utils/ImportGFFFastaUtil.py
+++ lib/kb_uploadmethods/Utils/ImportGFFFastaUtil.py
@@ -16,7 +16,7 @@ class ImportGFFFastaUtil:
     def __init__(self, config):
         self.callback_url = config['SDK_CALLBACK_URL']
         self.dfu = DataFileUtil(self.callback_url)
-        self.gfu = GenomeFileUtil(self.callback_url)
+        self.gfu = GenomeFileUtil(self.callback_url, service_ver='beta')
         self.uploader_utils = UploaderUtil(config)
 
     def import_gff_fasta_from_staging(self, params):
