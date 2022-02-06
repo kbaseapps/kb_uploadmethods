e824a0400508adcfc7e2d748d201f75447be10e7
Tianhao-Gu <tgu@anl.gov>
2022-02-05 20:21:04 -0600

    mock more file_to_shock

--- test/metagenome_gff_fasta_importer_test.py
+++ test/metagenome_gff_fasta_importer_test.py
@@ -5,6 +5,7 @@ import time
 import unittest
 from configparser import ConfigParser
 from os import environ
+import requests
 
 from biokbase.workspace.client import Workspace as workspaceService
 from mock import patch
@@ -14,6 +15,7 @@ from kb_uploadmethods.Utils.UploaderUtil import UploaderUtil
 from kb_uploadmethods.authclient import KBaseAuth as _KBaseAuth
 from kb_uploadmethods.kb_uploadmethodsImpl import kb_uploadmethods
 from kb_uploadmethods.kb_uploadmethodsServer import MethodContext
+from installed_clients.AbstractHandleClient import AbstractHandle as HandleService
 
 
 class kb_metagenome_uploadmethodsTest(unittest.TestCase):
@@ -46,14 +48,38 @@ class kb_metagenome_uploadmethodsTest(unittest.TestCase):
         cls.wsClient = workspaceService(cls.wsURL, token=cls.token)
         cls.serviceImpl = kb_uploadmethods(cls.cfg)
         cls.dfu = DataFileUtil(os.environ['SDK_CALLBACK_URL'], token=cls.token)
+        cls.hs = HandleService(url=cls.cfg['handle-service-url'],
+                               token=cls.token)
         cls.scratch = cls.cfg['scratch']
         cls.shockURL = cls.cfg['shock-url']
+
+        small_file = os.path.join(cls.scratch, 'test.txt')
+        with open(small_file, "w") as f:
+            f.write("empty content")
+        cls.test_shock = cls.dfu.file_to_shock({'file_path': small_file, 'make_handle': True})
+        cls.handles_to_delete = []
+        cls.nodes_to_delete = []
+        cls.handles_to_delete.append(cls.test_shock['handle']['hid'])
+        cls.nodes_to_delete.append(cls.test_shock['shock_id'])
 
     @classmethod
     def tearDownClass(cls):
         if hasattr(cls, 'wsName'):
             cls.wsClient.delete_workspace({'workspace': cls.wsName})
             print('Test workspace was deleted')
+        if hasattr(cls, 'nodes_to_delete'):
+            for node in cls.nodes_to_delete:
+                cls.delete_shock_node(node)
+        if hasattr(cls, 'handles_to_delete'):
+            cls.hs.delete_handles(cls.hs.hids_to_handles(cls.handles_to_delete))
+            print('Deleted handles ' + str(cls.handles_to_delete))
+
+    @classmethod
+    def delete_shock_node(cls, node_id):
+        header = {'Authorization': 'Oauth {0}'.format(cls.token)}
+        requests.delete(cls.shockURL + '/node/' + node_id, headers=header,
+                        allow_redirects=True)
+        print('Deleted shock node ' + node_id)
 
     def getWsClient(self):
         return self.__class__.wsClient
@@ -82,6 +108,12 @@ class kb_metagenome_uploadmethodsTest(unittest.TestCase):
         shutil.copy(os.path.join("data/test_Metagenome", fq_filename), fq_path)
 
         return {'copy_file_path': fq_path}
+
+    def mock_file_to_shock(params):
+        print('Mocking DataFileUtilClient.file_to_shock')
+        print(params)
+
+        return kb_metagenome_uploadmethodsTest().test_shock
 
     # def test_bad_upload_fasta_gff_file_params(self):
 
@@ -131,6 +163,7 @@ class kb_metagenome_uploadmethodsTest(unittest.TestCase):
 
     @patch.object(DataFileUtil, "download_staging_file", side_effect=mock_download_staging_file)
     @patch.object(UploaderUtil, "update_staging_service", return_value=None)
+    @patch.object(DataFileUtil, "file_to_shock", side_effect=mock_file_to_shock)
     def test_upload_metagenome_fasta_gff_file(self, download_staging_file, update_staging_service):
 
         fasta_file = "metagenome.fa"
